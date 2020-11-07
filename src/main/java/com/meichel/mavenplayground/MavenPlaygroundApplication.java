2ef208b567caffe9cdf6009a05b927917c08960e
quant500 <quanttrader500@gmail.com>
2020-11-08 00:21:08 +0100

    initial commit

---
+++ src/main/java/com/meichel/mavenplayground/MavenPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.meichel.mavenplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenPlaygroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MavenPlaygroundApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/meichel/mavenplayground/MavenPlaygroundApplicationTests.java
@@ -0,0 +1,14 @@
+package com.meichel.mavenplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenPlaygroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("Unit Tests are being executed.");
+    }
+
+}
