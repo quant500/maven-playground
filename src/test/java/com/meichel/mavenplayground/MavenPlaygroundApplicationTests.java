f10cda9ea9b197031e556c1f4b98aff0f9081e0a
quant500 <quanttrader500@gmail.com>
2020-11-08 10:37:37 +0100

    adapted pom.xml

--- src/test/java/com/meichel/mavenplayground/MavenPlaygroundApplicationTests.java
+++ src/test/java/com/meichel/mavenplayground/MavenPlaygroundApplicationTests.java
@@ -3,12 +3,14 @@ package com.meichel.mavenplayground;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.Instant;
+
 @SpringBootTest
 class MavenPlaygroundApplicationTests {
 
     @Test
     void contextLoads() {
-        System.out.println("Unit Tests are being executed.");
+        System.out.println("Unit Tests are being executed."+ Instant.now());
     }
 
 }
